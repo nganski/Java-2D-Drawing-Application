b6921a5bd6739f3e4842e2849623d36071c291c0
nganski <nganbui0330@gmail.com>
2026-03-09 18:25:43 -0400

    Add Main class for drawing application

---
+++ src/Main.java
@@ -0,0 +1,20 @@
+package pkg2ddrawingapplication;
+
+import java.awt.*;
+import javax.swing.*;
+
+/**
+ *
+ * @author Ngan
+ */
+public class Main {
+
+    
+    public static void main(String[] args) {
+        DrawingApplicationFrame frame = new DrawingApplicationFrame();
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setSize(650,500);
+        frame.setVisible(true);
+    }
+    
+}
